61b8a048a441e73a86ea1263d52ae89da4e4b68f
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2024-02-18 22:26:10 +0100

    add keymaps for lily58 and corne

---
+++ keyboards/splitkb/aurora/corne/keymaps/mesabloo/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define PERMISSIVE_HOLD_PER_KEY
+#define TAPPING_TERM_PER_KEY
+
+#if defined(OLED_ENABLE)
+#    if defined(OLED_FONT_H)
+#        undef OLED_FONT_H
+#    endif
+#    define OLED_FONT_H "./keymaps/mesabloo/glcdfont.c"
+#endif
+
+// Layers
+#define LAYER_ALPHA 0
+#define LAYER_NUM   1
+#define LAYER_SYMB  2
+#define LAYER_META  3
+#define LAYER_FN    4
+#define LAYER_GRV   5
+#define LAYER_ACC   6
+
+#define TRI_LAYER_LOWER_LAYER LAYER_NUM
+#define TRI_LAYER_UPPER_LAYER LAYER_SYMB
+#define TRI_LAYER_ADJUST_LAYER LAYER_META
---
+++ keyboards/splitkb/aurora/corne/keymaps/mesabloo/rules.mk
@@ -0,0 +1,17 @@
+# NOTE: These are already enabled by default at the revision level
+#ENCODER_ENABLE = yes
+OLED_ENABLE = yes
+
+# RGB Matrix is enabled at the revision level,
+# while we use the regular RGB underglow for testing
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = yes
+
+CONVERT_TO = liatris
+
+# Enable key overrides
+KEY_OVERRIDE_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
+
+TRI_LAYER_ENABLE = yes

8926d1dc3c5ae84575f88ab6d945972403384c6e
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2024-05-27 22:02:11 +0200

    introduce home row mods in keymaps

--- keyboards/splitkb/aurora/corne/keymaps/mesabloo/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/mesabloo/keymap.c
@@ -5,28 +5,35 @@
 #define FR_CART ALGR(FR_CCED)
 ////////////////////////////
 
+enum custom_keycodes {
+  MT_DIAE = SAFE_RANGE,
+  MT_LBRC,
+  MT_RBRC,
+  MT_BSLS,
+};
+
 #define BSPC_LOWR LT(LAYER_UNDEFINED, KC_BSPC)
 #define TAB_UPPR LT(LAYER_UNDEFINED, KC_TAB)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_ALPHA] = LAYOUT_split_3x6_3(
-        XXXXXXX, FR_Q, FR_W, FR_F,    FR_P,     FR_G,           /**/ FR_J,           FR_L,      FR_U,           FR_Y,    LALT_T(KC_ESC),        XXXXXXX,
-        XXXXXXX, FR_A, FR_R, FR_S,    FR_T,     FR_D,           /**/ FR_H,           FR_N,      FR_E,           FR_I,    FR_O,                  XXXXXXX,
-        XXXXXXX, FR_Z, FR_X, FR_C,    FR_V,     FR_B,           /**/ FR_K,           FR_M,      FR_COMM,        FR_SCLN, LT(LAYER_FN, FR_COLN), XXXXXXX,
-                             KC_LGUI, TAB_UPPR, LSFT_T(KC_SPC), /**/ LCTL_T(KC_ENT), BSPC_LOWR, RALT_T(KC_RCTL)
+        XXXXXXX, FR_Q,         FR_W,         FR_F,         FR_P,         FR_G,           /**/ FR_J,   FR_L,         FR_U,            FR_Y,         LALT_T(KC_ESC), XXXXXXX,
+        XXXXXXX, LGUI_T(FR_A), LALT_T(FR_R), LSFT_T(FR_S), LCTL_T(FR_T), FR_D,           /**/ FR_H,   LCTL_T(FR_N), RSFT_T(FR_E),    LALT_T(FR_I), RGUI_T(FR_O),   XXXXXXX,
+        XXXXXXX, FR_Z,         FR_X,         FR_C,         FR_V,         FR_B,           /**/ FR_K,   FR_M,         FR_COMM,         FR_SCLN,      FR_COLN,        XXXXXXX,
+                                             MO(LAYER_FN), TAB_UPPR,     LSFT_T(KC_SPC), /**/ KC_ENT, BSPC_LOWR,    RALT_T(KC_RCTL)
     ),
     [LAYER_NUM] = LAYOUT_split_3x6_3(
-        XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,  XXXXXXX,
-        XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, /**/ XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX,
-        XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_P0,   /**/ XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   XXXXXXX,
-                                   _______, _______, _______, /**/ _______,  _______, _______
+        XXXXXXX, XXXXXXX,         KC_P1,         KC_P2,         KC_P3,         XXXXXXX, /**/ XXXXXXX, XXXXXXX,         XXXXXXX,         XXXXXXX,       XXXXXXX,          XXXXXXX,
+        XXXXXXX, LGUI_T(XXXXXXX), LALT_T(KC_P4), LSFT_T(KC_P5), LCTL_T(KC_P6), XXXXXXX, /**/ XXXXXXX, LCTL_T(KC_LEFT), RSFT_T(KC_DOWN), LALT_T(KC_UP), RGUI_T(KC_RIGHT), XXXXXXX,
+        XXXXXXX, XXXXXXX,         KC_P7,         KC_P8,         KC_P9,         KC_P0,   /**/ XXXXXXX, KC_HOME,         KC_PGDN,         KC_PGUP,       KC_END,           XXXXXXX,
+                                                 _______,       _______,       _______, /**/ _______,  _______, _______
     ),
     [LAYER_SYMB] = LAYOUT_split_3x6_3(
-        XXXXXXX, FR_EXLM, FR_AT,   FR_HASH, FR_DLR,         FR_PERC,        /**/ FR_CART, FR_AMPR, FR_LPRN, FR_RPRN, FR_ASTR, XXXXXXX,
-        XXXXXXX, FR_LABK, FR_DIAE, FR_CIRC, OSL(LAYER_ACC), OSL(LAYER_GRV), /**/ FR_MINS, FR_EQL,  FR_LBRC, FR_RBRC, FR_BSLS, XXXXXXX,
-        XXXXXXX, FR_GRV,  FR_QUOT, FR_CCED, XXXXXXX,        FR_TILD,        /**/ FR_UNDS, FR_PLUS, FR_LCBR, FR_RCBR, FR_PIPE, XXXXXXX,
-                                   _______, _______,        _______,        /**/ _______, _______, _______
+        XXXXXXX, FR_EXLM,         FR_AT,           FR_HASH,         FR_DLR,          FR_PERC,        /**/ FR_CART, FR_AMPR,        FR_LPRN,         FR_RPRN,         FR_ASTR,         XXXXXXX,
+        XXXXXXX, LGUI_T(FR_LABK), LALT_T(MT_DIAE), LSFT_T(FR_CIRC), LCTL_T(FR_QUOT), FR_TILD,        /**/ FR_MINS, LCTL_T(FR_EQL), RSFT_T(MT_LBRC), LALT_T(MT_RBRC), RGUI_T(MT_BSLS), XXXXXXX,
+        XXXXXXX, XXXXXXX,         FR_GRV,          FR_CCED,         OSL(LAYER_ACC),  OSL(LAYER_GRV), /**/ FR_UNDS, FR_PLUS,        FR_LCBR,         FR_RCBR,         FR_PIPE,         XXXXXXX,
+                                                   _______,          _______,        _______,        /**/ _______, _______,        _______
     ),
     [LAYER_META] = LAYOUT_split_3x6_3(
         XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -35,10 +42,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    XXXXXXX, _______, XXXXXXX, /**/ XXXXXXX, _______, XXXXXXX
     ),
     [LAYER_FN] = LAYOUT_split_3x6_3(
-        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, /**/ XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,
-        XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM,
-        XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, /**/ XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, _______, XXXXXXX,
-                                   _______, XXXXXXX, _______, /**/ _______, XXXXXXX, _______
+        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, /**/ XXXXXXX, KC_PSCR,         XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_CAPS,
+        XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, /**/ XXXXXXX, LCTL_T(XXXXXXX), RSFT_T(XXXXXXX), LALT_T(XXXXXXX), RGUI_T(XXXXXXX), KC_NUM,
+        XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, /**/ XXXXXXX, KC_MUTE,         KC_VOLD,         KC_VOLU,         _______,         XXXXXXX,
+                                   _______, XXXXXXX, _______, /**/ _______, XXXXXXX,         _______
     ),
     [LAYER_GRV] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, FR_UGRV, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -83,6 +90,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             return true;
+        case LALT_T(MT_DIAE): 
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(FR_DIAE); 
+                return false;
+            }
+            return true;
+        case RSFT_T(MT_LBRC):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(FR_LBRC); 
+                return false;
+            }
+            return true;        
+        case LALT_T(MT_RBRC):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(FR_RBRC); 
+                return false;
+            }
+            return true;
+        case RGUI_T(MT_BSLS): 
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(FR_BSLS); 
+                return false;
+            }
+            return true;
         default:
             return true;
     }
@@ -105,9 +136,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #if defined(PERMISSIVE_HOLD_PER_KEY)
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LSFT_T(KC_SPC):
-        case LCTL_T(KC_ENT):
-        case LALT_T(KC_ESC):
         case RALT_T(KC_LCTL):
             return true;
         default:
@@ -131,8 +159,6 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 #if defined(RETRO_TAPPING_PER_KEY)
 bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(LAYER_FN, FR_COLN):
-            return true;
         default:
             return false;
     }
@@ -142,9 +168,6 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
 #if defined(TAPPING_TERM_PER_KEY)
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LSFT_T(KC_SPC):
-        case LCTL_T(KC_ENT):
-            // return TAPPING_TERM + 75;
         default:
             return TAPPING_TERM;
     }

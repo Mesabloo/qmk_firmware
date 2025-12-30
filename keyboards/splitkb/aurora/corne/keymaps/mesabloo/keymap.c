d0f9bd0325c424c2461af1b669542d415e14493b
Ghilain <22964017+Mesabloo@users.noreply.github.com>
2025-12-30 22:15:01 +0100

    fix behavior of some keys

--- keyboards/splitkb/aurora/corne/keymaps/mesabloo/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/mesabloo/keymap.c
@@ -154,6 +154,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case RALT_T(KC_LCTL):
+        case LSFT_T(KC_ENT):
             return true;
         default:
             return false;
@@ -210,7 +211,10 @@ bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
                       uint16_t other_keycode, keyrecord_t* other_record) {
     // Exceptionally allow some one-handed chords for hotkeys.
     switch (tap_hold_keycode) {
-        case LSFT_T(KC_ENT): return false;
+        case LSFT_T(KC_ENT):
+        case TAB_UPPR:
+        case BSPC_LOWR:
+            return true;
     }
     // Otherwise defer to the opposite hands rule.
     return get_chordal_hold_default(tap_hold_record, other_record);
